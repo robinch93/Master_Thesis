0c74a9acd0b449e7f08e5f49e888e809d964c1d3
robin <34096241+robinch93@users.noreply.github.com>
2022-08-17 02:42:15 +0200

    unit converter scripts updated

--- app/src/test/java/demo/apps/unitConverterApp/unitConverterAppInputScripts/UnitConverterAppium3.java
+++ app/src/test/java/demo/apps/unitConverterApp/unitConverterAppInputScripts/UnitConverterAppium3.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -20,33 +21,30 @@ public class UnitConverterAppium3 {
 		// Created object of DesiredCapabilities class.
 		DesiredCapabilities dc = new DesiredCapabilities();
 
-		dc.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
-		dc.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-		dc.setCapability(MobileCapabilityType.PLATFORM_VERSION, "13");
-		dc.setCapability(MobileCapabilityType.DEVICE_NAME, "pixel4");
+		dc.setCapability(MobileCapabilityType.AUTOMATION_NAME, Globals.AUTOMATION_NAME);
+		dc.setCapability(MobileCapabilityType.PLATFORM_NAME, Globals.PLATFORM_NAME);
+		dc.setCapability(MobileCapabilityType.PLATFORM_VERSION, Globals.PLATFORM_VERSION);
+		dc.setCapability(MobileCapabilityType.DEVICE_NAME, Globals.DEVICE_NAME);
 		dc.setCapability(MobileCapabilityType.APP, Globals.cwd + "/apks/unit-converter.apk");
+		dc.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, "3000");
 
 		URL url = new URL("http://127.0.0.1:4723/wd/hub");
 
 		AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(url, dc);
-		Thread.sleep(3000);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
 		driver.findElementByXPath("//android.widget.TextView[@text='Speed']").click();
 
-		Thread.sleep(200);
 		driver.findElementById("com.androidapps.unitconverter:id/chip_to_unit_name").click();
 
-		Thread.sleep(200);
 		driver.findElementByXPath("//android.widget.TextView[@text='meter/second']").click();
 
-		Thread.sleep(200);
 		driver.findElementById("com.androidapps.unitconverter:id/bt_simple_units_view").click();
 
-		Thread.sleep(200);
+		driver.findElementById("com.androidapps.unitconverter:id/et_from_unit_val").click();
 		driver.findElementById("com.androidapps.unitconverter:id/et_from_unit_val").clear();
 		driver.findElementById("com.androidapps.unitconverter:id/et_from_unit_val").sendKeys("5");
 
-		Thread.sleep(200);
 		assertTrue(driver.findElementByXPath("//*[contains(@text,'1.389')]").isDisplayed());
 
 	}
--- app/src/test/java/demo/apps/unitConverterApp/unitConverterAppUpdatedScripts/UnitConverterAppium3Updated.java
+++ app/src/test/java/demo/apps/unitConverterApp/unitConverterAppUpdatedScripts/UnitConverterAppium3Updated.java
@@ -1,14 +1,15 @@
 package demo.apps.unitConverterApp.unitConverterAppUpdatedScripts;
-import demo.parser.AddScreenshot;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import demo.parser.AddScreenshot;
 import demo.parser.Globals;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
@@ -21,55 +22,44 @@ public class UnitConverterAppium3Updated {
 		// Created object of DesiredCapabilities class.
 		DesiredCapabilities dc = new DesiredCapabilities();
 
-		dc.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
-		dc.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-		dc.setCapability(MobileCapabilityType.PLATFORM_VERSION, "13");
-		dc.setCapability(MobileCapabilityType.DEVICE_NAME, "pixel4");
+		dc.setCapability(MobileCapabilityType.AUTOMATION_NAME, Globals.AUTOMATION_NAME);
+		dc.setCapability(MobileCapabilityType.PLATFORM_NAME, Globals.PLATFORM_NAME);
+		dc.setCapability(MobileCapabilityType.PLATFORM_VERSION, Globals.PLATFORM_VERSION);
+		dc.setCapability(MobileCapabilityType.DEVICE_NAME, Globals.DEVICE_NAME);
 		dc.setCapability(MobileCapabilityType.APP, Globals.cwd + "/apks/unit-converter.apk");
+		dc.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, "3000");
 
 		URL url = new URL("http://127.0.0.1:4723/wd/hub");
 
 		AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(url, dc);
-		Thread.sleep(3000);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
-		MobileElement element0 = driver.findElementByXPath("//android.widget.TextView[@text='Speed']");
-		AddScreenshot.elementScreenshot("UnitConverterAppium3", driver, element0 , "element0");
+		MobileElement element1 = driver.findElementByXPath("//android.widget.TextView[@text='Speed']");
+		AddScreenshot.elementScreenshot("UnitConverterAppium3", driver, element1, "element1");
 		driver.findElementByXPath("//android.widget.TextView[@text='Speed']").click();
 
-
-		Thread.sleep(200);
-		MobileElement element1 = driver.findElementById("com.androidapps.unitconverter:id/chip_to_unit_name");
-		AddScreenshot.elementScreenshot("UnitConverterAppium3", driver, element1 , "element1");
+		MobileElement element2 = driver.findElementById("com.androidapps.unitconverter:id/chip_to_unit_name");
+		AddScreenshot.elementScreenshot("UnitConverterAppium3", driver, element2, "element2");
 		driver.findElementById("com.androidapps.unitconverter:id/chip_to_unit_name").click();
 
-
-		Thread.sleep(200);
-		MobileElement element2 = driver.findElementByXPath("//android.widget.TextView[@text='meter/second']");
-		AddScreenshot.elementScreenshot("UnitConverterAppium3", driver, element2 , "element2");
+		MobileElement element3 = driver.findElementByXPath("//android.widget.TextView[@text='meter/second']");
+		AddScreenshot.elementScreenshot("UnitConverterAppium3", driver, element3, "element3");
 		driver.findElementByXPath("//android.widget.TextView[@text='meter/second']").click();
 
-
-		Thread.sleep(200);
-		MobileElement element3 = driver.findElementById("com.androidapps.unitconverter:id/bt_simple_units_view");
-		AddScreenshot.elementScreenshot("UnitConverterAppium3", driver, element3 , "element3");
+		MobileElement element4 = driver.findElementById("com.androidapps.unitconverter:id/bt_simple_units_view");
+		AddScreenshot.elementScreenshot("UnitConverterAppium3", driver, element4, "element4");
 		driver.findElementById("com.androidapps.unitconverter:id/bt_simple_units_view").click();
 
-
-		Thread.sleep(200);
-		MobileElement element4 = driver.findElementById("com.androidapps.unitconverter:id/et_from_unit_val");
-		AddScreenshot.elementScreenshot("UnitConverterAppium3", driver, element4 , "element4");
-		driver.findElementById("com.androidapps.unitconverter:id/et_from_unit_val").clear();
-
 		MobileElement element5 = driver.findElementById("com.androidapps.unitconverter:id/et_from_unit_val");
-		AddScreenshot.elementScreenshot("UnitConverterAppium3", driver, element5 , "element5");
+		AddScreenshot.elementScreenshot("UnitConverterAppium3", driver, element5, "element5");
+		driver.findElementById("com.androidapps.unitconverter:id/et_from_unit_val").click();
+
+		driver.findElementById("com.androidapps.unitconverter:id/et_from_unit_val").clear();
 		driver.findElementById("com.androidapps.unitconverter:id/et_from_unit_val").sendKeys("5");
 
-
-		Thread.sleep(200);
-		MobileElement element6 = driver.findElementByXPath("//*[contains(@text,'1.389')]");
-		AddScreenshot.elementScreenshot("UnitConverterAppium3", driver, element6 , "element6");
+		MobileElement element6 = driver.findElementById("com.androidapps.unitconverter:id/et_from_unit_val");
+		AddScreenshot.elementScreenshot("UnitConverterAppium3", driver, element6, "element6");
 		assertTrue(driver.findElementByXPath("//*[contains(@text,'1.389')]").isDisplayed());
-
 
 	}
 
